ab781a143ec9513a5cd29124791946124242840f
S3niorMCYT <77205348+SeniorMCYT@users.noreply.github.com>
2021-12-29 00:23:25 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 chmod +x ./yt-dlp
 sudo apt install ffmpeg
-nohup ./yt-dlp https://www.twitch.tv/taha_immortal &
+nohup ./yt-dlp https://www.twitch.tv/IRGeneral &
 sleep 60
 pkill yt-dlp
