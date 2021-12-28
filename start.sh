ee94679dc5e0d10d0e5a222461a1ec76053a3ebb
S3niorMCYT <77205348+SeniorMCYT@users.noreply.github.com>
2021-12-28 21:05:42 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 chmod +x ./yt-dlp
-wget https://github.com/FFmpeg/FFmpeg/releases/download/n3.0/ffmpeg-3.0.tar.gz
-tar xfv ffmpeg-3.0.tar.gz
-ls
+sudo apt install ffmpeg
 ./yt-dlp https://twitch.tv/FeLORDO
